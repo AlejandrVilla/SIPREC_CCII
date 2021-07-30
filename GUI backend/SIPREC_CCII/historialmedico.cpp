90255058680af3a83b2f79f5635899bad2253af4
SamuelChambiYtusaca <82793718+SamuelChambiYtusaca@users.noreply.github.com>
2021-07-30 14:37:30 -0500

    Delete GUI backend directory

--- GUI backend/SIPREC_CCII/historialmedico.cpp
+++
@@ -1,6 +0,0 @@
-#include "historialmedico.h"
-
-void HistorialMedico::setEnfermedades( QString enf ){
-    enfermedadesPrevias.push_back(enf);
-}
-
--- GUI backend/SIPREC_CCII/mascarilla.cpp
+++
@@ -1,9 +0,0 @@
-#include "mascarilla.h"
-
-void Mascarilla::setCalidad(QString calidad){
-    this->calidad = calidad;
-}
-
-QString Mascarilla::getCalidad(){
-    return calidad;
-}
