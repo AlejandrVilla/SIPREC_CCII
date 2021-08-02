01cb853ec2fa1ad1acd4e5e7c4402fc6fd79640d
AlejandrVilla <avillah@unsa.edu.pe>
2021-08-01 20:47:57 -0500

    última actualización :'D

---
+++ v1.15/main.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+#include "centro_salud.h"
+#include "datavisitor.h"
+#include <QApplication>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    /*
+     *Implementar metodo para obtener la ruta local.
+     *Implementacion de patrones(Importante, falta uno de 4).
+    */
+
+    QApplication a(argc, argv);
+    Centro_Salud *centroSalud = Centro_Salud::getInstance(nullptr,"Honorio Delgado");   //singleton patern
+    DataVisitor visitCentroSalud(centroSalud);                                          //visitor patern
+    a.setStyle("fusion");
+    MainWindow w(nullptr,centroSalud);
+    w.show();
+    return a.exec();
+}
